821979ca0b640a8ccd9cb7f62e73413636668a96
Tetiana98 <tatyanamartseniuk98@gmail.com>
2023-05-12 19:06:37 +0300

    Lesson7

--- Lesson7/Task1.py
+++ Lesson7/Task1.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 dir_path = os.path.dirname(os.path.realpath('test_file.csv'))
 file_path = os.path.join(dir_path, 'test_file.csv')
-exchange_rate = 36
+exchange_rate = 37
 data = pd.read_csv(file_path)
 for column in data:
     data[column] = data[column] * exchange_rate
