c9d0861b0ae6885e09391c9eb33ad2dbb01ebb5f
Tetiana98 <tatyanamartseniuk98@gmail.com>
2023-05-16 21:49:48 +0300

    Lesson9

--- Lesson9/Lesson9.py
+++ Lesson9/Lesson9.py
@@ -1,35 +1,37 @@
-#Task1
+# Task1
+import json
+def max_women_from_1977():
+    with open('group_people.json') as file:
+        data = json.load(file)
+    max_count = 0
+    group_id = 0
+    for group in data:
+        count = 0
+        for person in group['people']:
+            if person['gender'] == 'Female' and person['year'] > 1977:
+                count += 1
+        if count > max_count:
+            max_count = count
+            group_id = group['id_group']
+    print('ID of the found group:', group_id)
+    print('Number of women born after 1977:', max_count)
+
+# Task2
 import json
 
-with open('group_people.json') as file:
-    data = json.load(file)
-max_count = 0
-group_id = 0
-for group in data:
-    count = 0
-    for person in group['people']:
-        if person['gender'] == 'Female' and person['year'] > 1997:
-            count += 1
-    if count > max_count:
-        max_count = count
-        group_id = group['id_group']
-print('ID of the found group:', group_id)
-print('Number of women born after 1977:', max_count)
+def find_the_most_successful_manager():
+    with open('manager_sales.json') as file:
+        data = json.load(file)
+    successful_manager = None
+    max_total_sales = 0
 
-#Task2
-import json
+    for manager_data in data:
+        amount_sales = sum([car['price'] for car in manager_data['cars']])
+        if amount_sales > max_total_sales:
+            max_total_sales = amount_sales
+            successful_manager = f"{manager_data['manager']['first_name']} {manager_data['manager']['last_name']}"
+    print(successful_manager, max_total_sales)
 
-with open('manager_sales.json') as file:
-    data = json.load(file)
-successful_manager = None
-max_total_sales = 0
-
-for manager_data in data:
-    amount_sales = sum([car['price'] for car in manager_data['cars']])
-    if amount_sales > max_total_sales:
-        max_total_sales = amount_sales
-        successful_manager = f"{manager_data['manager']['first_name']} {manager_data['manager']['last_name']}"
-
-print(successful_manager, max_total_sales)
-
-
+if __name__ == '__main__':
+    max_women_from_1977()
+    find_the_most_successful_manager()
